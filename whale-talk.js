543d2b09a11eed87c9cccba6bef7c5d36eba7dee
mhammjr <69519372+mhammjr@users.noreply.github.com>
2020-09-04 23:29:40 -0500

    Create whale-talk.js
    A small loops practice project from codecademy.

---
+++ whale-talk.js
@@ -0,0 +1,19 @@
+let input = 'turpentine and turtles';
+const vowels = ["a", "e", "i", "o", "u"];
+let resultArray = [];
+
+for(i = 0; i < input.length; i++) {
+    //console.log(i)
+    for(j = 0; j < vowels.length; j++) {
+      if (input[i] === vowels[j]) {
+        resultArray.push(input[i])
+      }
+    }
+  if (input[i] === 'e')  {
+   resultArray.push(input[i])
+ }   
+  if (input[i] === 'u')  {
+   resultArray.push(input[i])
+ } 
+};
+console.log(resultArray.join('').toUpperCase());
